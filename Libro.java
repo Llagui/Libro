2ff782a6880a2c39ef2ddd1b2ec50a28fa177b06
Yago <nietogarridoyago@gmail.com>
2021-11-09 09:40:50 +0100

    1847.03 añadir Nº de paginas y metodo getter

--- Libro.java
+++ Libro.java
@@ -11,14 +11,16 @@ public class Libro {
 
     private String autor;
     private String titulo;
+    private int numPaginas;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int numeroPaginas)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        numPaginas = numeroPaginas;
     }
     
     public String getAutor()
@@ -29,6 +31,11 @@ public class Libro {
     public String getTitulo()
     {
         return titulo;
+    }
+    
+    public int getnumeroPaginas()
+    {
+        return numPaginas;
     }
     
     public void imprimirAutor()
